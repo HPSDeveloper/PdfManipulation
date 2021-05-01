b70a7a178e43e7b1b31409fa0b2376a4a9d3d150
hs.a@talsicht.ch <hs.a@talsicht.ch>
2021-05-01 16:38:32 +0200

    Split point/name definitions can be defined in a file now.

--- src/main/java/ch/hps/pdf/main/SplitPdf.java
+++ src/main/java/ch/hps/pdf/main/SplitPdf.java
@@ -9,19 +9,23 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import java.io.File;
 import java.io.IOException;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class SplitPdf {
     private static String DATE_PATTERN = "yyyy.MM.dd";
     private static String BASE_DIR = "C:\\tmp\\zahlungen\\";
+    private static String IN_FILE_NAME = "IMG_20210501_0001.pdf";
 
     public static void main(String[] args) throws IOException {
 
         String dateInString =new SimpleDateFormat(DATE_PATTERN).format(new Date());
 
         //Loading an existing PDF document
-        File file = new File(BASE_DIR + "IMG_20210501_0001.pdf");
+        File file = new File(BASE_DIR + IN_FILE_NAME);
         PDDocument doc = PDDocument.load(file);
 
         //Instantiating Splitter class
@@ -55,9 +59,23 @@ public class SplitPdf {
     }
 
     private static List<SplitDef> getSplitMap(int size) {
-        List<SplitDef> splitDefs = ImmutableList.of(new SplitDef("Busse", 1), new SplitDef("Abo Klaex", 3), new SplitDef("Musikschule Kristin", 4));
+        List<SplitDef> splitDefs = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(BASE_DIR + replaceExtension(IN_FILE_NAME, "txt")))) {
+            stream.forEach(line -> {
+                System.out.println(line);
+                splitDefs.add(SplitDef.fromString(line));
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        List<SplitDef> splitDefs = ImmutableList.of(new SplitDef("Busse", 1), new SplitDef("Abo Klaex", 3), new SplitDef("Musikschule Kristin", 4));
         convertToEndPageNuberRefs(splitDefs, size);
         return splitDefs;
+    }
+
+    private static String replaceExtension(String inFileName, String extension) {
+        return com.google.common.io.Files.getNameWithoutExtension(inFileName) + "." + extension;
     }
 
     private static void convertToEndPageNuberRefs(List<SplitDef> splitDefs, int size) {
@@ -67,28 +85,38 @@ public class SplitPdf {
     }
 
     private static class SplitDef {
-        public String getDocName() {
+        String getDocName() {
             return docName;
         }
 
-        public int getPageIdx() {
+        int getPageIdx() {
             return pageIdx;
         }
 
         private String docName;
 
-        public void setPageIdx(int pageIdx) {
+        void setPageIdx(int pageIdx) {
             this.pageIdx = pageIdx;
         }
 
         private int pageIdx;
 
-        public SplitDef(String docName, int pageIdx) {
+        SplitDef(String docName, int pageIdx) {
             this.docName = docName;
             this.pageIdx = pageIdx;
         }
 
-
+        static SplitDef fromString(String s){
+            String[] nameAndIndex = s.split(";");
+            if(nameAndIndex.length == 2){
+                try {
+                    return new SplitDef(nameAndIndex[0], Integer.valueOf(nameAndIndex[1]));
+                }catch(NumberFormatException ex){
+                    throw new RuntimeException("ERROR: The split config TXT file must contain have format '<section name>;<page number>'", ex);
+                }
+            }
+            throw new RuntimeException("ERROR: The split config TXT file must contain have format '<section name>;<page number>'");
+        }
     }
 }
 
